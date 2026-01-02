a709b1d852ac1ca3d4d61eaa38347fa37c93bcf3
woojeong <dayontheride@gmail.com>
2026-01-02 18:07:13 +0900

    Form.vue 01.02

--- vue/vue-memo/src/App.vue
+++ vue/vue-memo/src/App.vue
@@ -3,9 +3,31 @@
 </script>
 
 <template>
+<div class="app">
+    <!-- 부트스트랩은 container 클래스가 부모 역할
+    py-4: padding-top, padding-bottom: 4. 아마도 24px
+    -->
+    <div class="container py-4">
+        <!-- 헤더 -->
+    <header class="pb-4 mb-4 border-bottom">
+            <router-link to="/" class="text-dark text-decoration-none">
+                <!-- fs: font-size: 4-->
+                <b class="fs-4">Memo Application</b>
+            </router-link>
+    </header>
+    <!-- 메인-->
+    <main>
+        <router-view/>
+    </main>
 
+    <!--푸터-->
+    <footer class="pt-e mt-4 border-top">
+        &copy; 2024. Memo. All rights reserved.
+    </footer>
+    </div>
+</div>
 </template>
 
 <style scoped>
-
+.app .container { max-width: 576px; }
 </style>
--- vue/vue-memo/src/router/index.js
+++ vue/vue-memo/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
+import Form from '@/views/Form.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +9,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: HomeView,
     },
-  
+    {
+      path: '/memos/add',
+      name: 'add',
+      component: () => import ('../views/Form.vue')
+    },
+    {
+      path: '/memos/:id', // path variable (경로 변수)
+      component: () => import ('../views/Form.vue')
+    },
   ],
 })
 
